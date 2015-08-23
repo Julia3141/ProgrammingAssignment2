7664cb334477eac2c1b07effcf012329575e20fb
Julia3141 <juliaselbe@gmail.com>
2015-08-24 00:09:01 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(X = matrix()) {
+inverse <- NULL
+set <- function(Y){
+	X <<- Y
+	inverse <<- NULL
+	}
+get <- function() X
+setinverse <- function(Inverse) inverse <<- Inverse
+getinverse <- function() inverse
+list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+}
 
-## Write a short comment describing this function
+## NB: cacheSolve uses corpcor- this function will attempt to load copcor library 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+cacheSolve <- function(X, ...) 
+{
+if(require("corpcor")){
+	print("corpcor is loaded correctly")
+	} else {
+		print("trying to install corpcor")
+		install.packages("corpcor")
+		if(require(corpcor)){
+			print("corpcor installed and loaded")
+			} else {
+			stop("could not install corpcor")
+			}
+		}
+inverse <- X$getinverse()
+if(!is.null(inverse)){
+	message("matrix is in memory")
+	return(inverse)
+	}
+message("inverse is not in memory so the inverse (if exist) is gonna be computed")
+data <- X$get()
+inverse <- pseudoinverse(data, ...)
+X$setinverse(inverse)
+inverse
 }
 
 
-## Write a short comment describing this function
+#Test1- square matrix
+X <- matrix(rpois(25,3), nrow = 5)
+cX <- makeCacheMatrix(X)
+cX$get()
+cacheSolve(cX)
+cacheSolve(cX)
+invX <- cacheSolve(cX)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#Test2- rectangular matrix rows > cols
+Y <- matrix(rpois(20,2), nrow = 5, ncol = 4)
+cY <- makeCacheMatrix(Y)
+cY$get()
+cacheSolve(cY)
+cacheSolve(cY)
+invY <- cacheSolve(cY)
+
+#test3- rectangular matrix rows < cols
+Z <- matrix(rpois(20,1), nrow = 4, ncol = 5)
+cZ <- makeCacheMatrix(Z)
+cZ$get()
+cacheSolve(cZ)
+cacheSolve(cZ)
+invZ <- cacheSolve(cZ)
+
+#test4 multiplication must return identity or closer
+invX %*% X 
+X %*% invX
+invY %*% Y 
+Z %*% invZ 
